d063485b01e599a8e6ef2234ba650e7c63e78e05
my-amtech <ammuftau74@gmail.com>
2023-01-28 18:45:20 +0100

    forget password commit

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -11,12 +11,12 @@ const Profile = ({ isProfile }) => {
     <div className={`${isProfile ? 'flex' : 'hidden'} right-0 top-[70px] absolute rounded-md mr-2 bg-white z-[995] transition-all duration-300  place-items-center`}>
       {user ?
         <div className='p-2'>
-          <h1>{user.user.user?.pic}</h1>
+          <h1>{user?.user?.user?.pic}</h1>
           <ion-icon name="person-circle-outline" size="large"></ion-icon>
-          <h1 className='text-xl font-semibold'>{user.user.name}</h1>
+          <h1 className='text-xl font-semibold'>{user?.user?.name}</h1>
           <hr />
-          <h1 className='text-lg'>{user.user.email || "musa@gmail.com"}</h1>
-          <h1 className='text-lg'>{user.user.phone}</h1>
+          <h1 className='text-lg'>{user?.user?.email || "musa@gmail.com"}</h1>
+          <h1 className='text-lg'>{user?.user?.phone}</h1>
           <button onClick={() => logout()} className="text-xl lg:text-2xl text-red-400 cursor-pointer">Logout</button>
         </div> : ''}
     </div>
